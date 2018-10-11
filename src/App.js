a2bda5ed9e09232c2e2beb877161b94179b3cedb
Serina Ko <serinako08@gmail.com>
2018-10-11 16:33:47 +1100

    handle authentication events

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 class App extends Component {
+  state = {
+    loggedIn: false
+  };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBj83zdrKZxOp9Xb-xdgebvr5DxuLZV-HE',
@@ -12,6 +15,14 @@ class App extends Component {
       projectId: 'authentication-f1cc3',
       storageBucket: 'authentication-f1cc3.appspot.com',
       messagingSenderId: '712035064838'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
   render() {
